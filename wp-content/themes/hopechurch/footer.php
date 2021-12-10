4fea915e113b797bfd0682640ba02a35a04d9b5a
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-12-10 09:14:59 +0000

    initial commit

---
+++ wp-content/themes/hopechurch/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package hopechurch
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="https://www.ninefootone.co.uk/">Logo by ninefootone creative</a>
+			<span class="sep"> | </span>
+			<a href="https://www.jackbarber.co.uk">Website by Jack Barber</a>	
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
